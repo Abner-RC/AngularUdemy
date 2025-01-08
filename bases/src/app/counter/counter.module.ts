2c347dfb0eed2a26e22df738e52198be79739516
AbnerRC <abnerramirezcastaneda412@gmail.com>
2025-01-08 13:01:00 -0800

    Bases Counter (Placed it into a module)

---
+++ bases/src/app/counter/counter.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CounterComponent } from "./components/counter/counter.component";
+
+@NgModule({
+  imports: [
+    CounterComponent
+  ],
+  exports: [
+    CounterComponent
+  ]
+})
+export class CounterModule {
+}
