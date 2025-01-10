fde249e4774dd4f96db2606c3901588eb5193e60
AbnerRC <abnerramirezcastaneda412@gmail.com>
2025-01-10 09:04:39 -0800

    Bases DBZ (Use the character's ID instead of the loop index)

--- bases/src/app/dbz/components/list/list.component.ts
+++ bases/src/app/dbz/components/list/list.component.ts
@@ -6,16 +6,18 @@ import { Character } from '../../interfaces/character.interface';
   selector: 'dbz-list',
   standalone: false,
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
   @Input()
   public characterList: Character[] = [];
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
+  public onDelete: EventEmitter<string> = new EventEmitter();
 
-  onDeleteCharacter(index: number):void {
-    this.onDelete.emit(index);
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+    console.log(id);
+    this.onDelete.emit(id);
   }
 }
